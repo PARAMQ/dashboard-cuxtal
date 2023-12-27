856c6ffdb2493f702d426d09ae90c2f390493b49
DJCrespo <dj.crespo.castilla@gmail.com>
2023-12-27 08:40:56 -0600

    Updates

--- _nuxt/d1bbcb2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{1199:function(t,n,e){"use strict";e.r(n);var r=e(1198);n.default={name:"line-chart",extends:r.a,mixins:[r.b.reactiveProp],props:{extraOptions:Object},data:function(){return{ctx:null}},mounted:function(){var t=this;this.$watch("chartData",(function(n,e){e||t.renderChart(t.chartData,t.extraOptions)}),{immediate:!0})}}}}]);
